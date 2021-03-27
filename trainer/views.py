871b6605e77b068ce692d68efe415e4ebdcf33b2
Korzhak <navchalkin@ex.ua>
2021-03-28 00:32:58 +0200

    added showing users data

--- trainer/views.py
+++ trainer/views.py
@@ -7,25 +7,39 @@ def home_view(request):
 
 @csrf_exempt
 def user_view(request):
+    context = {}
     if request.method == "POST":
         print(request.POST)
+        context["method"] = "POST"
+        for value in request.POST:
+            context[value] = request.POST[value]
 
     elif request.method == "GET":
         print(request.GET)
+        context["method"] = "GET"
+        for value in request.GET:
+            context[value] = request.GET[value]
 
     else:
         print("Mistake")
-    return render(request, "trainer/user.html", {})
+    return render(request, "trainer/user.html", context)
 
 
 @csrf_exempt
 def comment_view(request):
+    context = {}
     if request.method == "POST":
         print(request.POST)
+        context["method"] = "POST"
+        for value in request.POST:
+            context[value] = request.POST[value]
 
     elif request.method == "GET":
         print(request.GET)
+        context["method"] = "GET"
+        for value in request.GET:
+            context[value] = request.GET[value]
 
     else:
         print("Mistake")
-    return render(request, "trainer/comment.html", {})
+    return render(request, "trainer/comment.html", context)
